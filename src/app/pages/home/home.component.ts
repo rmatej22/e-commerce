6d56aca70dee6cf27e141b163664cd2ee3205eda
rmatej22 <rmatej545@gmail.com>
2023-02-02 17:48:07 +0100

    add filtering by categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   getProducts(): void {
     this.productsSubscription = this.storeService
-      .getAllProducts(this.count, this.sort)
+      .getAllProducts(this.count, this.sort, this.category)
       .subscribe((_products) => {
         this.products = _products;
       });
@@ -46,6 +46,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+    this.getProducts();
   }
 
   onAddToCart(product: Product): void {
