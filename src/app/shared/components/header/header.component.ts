2dbc2d631744de3b61bf535cd56bdd72be1ce033
rmatej22 <rmatej545@gmail.com>
2023-02-02 15:11:10 +0100

    add header functionality for card items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
   selector: "app-header",
@@ -6,7 +8,31 @@ import { Component, OnInit } from "@angular/core";
   styles: [],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {}
+
+  getTotal(items: CartItem[]): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClear() {
+    this.cartService.clearCart();
+  }
 }
