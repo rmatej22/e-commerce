0748fe493a6c8db15b0366779cec31de0366ffe1
rmatej22 <rmatej545@gmail.com>
2023-03-15 20:14:56 +0100

    add meta tags to app.component
    feature/add-ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
 import { Cart } from "./models/cart";
 import { CartService } from "./services/cart.service";
 
@@ -15,9 +16,30 @@ export class AppComponent implements OnInit {
     items: [],
   };
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private title: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
+    this.title.setTitle("E-commerce store");
+
+    this.meta.addTags([
+      {
+        name: "author",
+        content: "Matej",
+      },
+      {
+        name: "keywords",
+        content: "e-commerce",
+      },
+      {
+        name: "description",
+        content: "Choose and buy your favorite products at the best prices!",
+      },
+    ]);
+
     this.cartService.cart.subscribe((_cart) => {
       this.cart = _cart;
     });
